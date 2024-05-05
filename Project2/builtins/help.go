660fa4110cdeaec1d73db359987f8071cb9e7351
Alexckes <108437954+Alexckes@users.noreply.github.com>
2024-05-05 04:42:01 +0000

    helped

--- Project2/builtins/help.go
+++ Project2/builtins/help.go
@@ -5,6 +5,6 @@ import (
 )
 
 func Help(args ...string) error {
-	fmt.Println("The commands are:\ntimes: shows the current time.\ncd: go to home directory.\nenv:")
+	fmt.Println("The commands are:\ntimes: shows the current time.\ncd: goes to the home directory.\nenv [wwriter]: writes the environment.\nhelp: shows the commands.\neval [arg]: executes the argument command.\npwd: returns the home directory.\necho [arg]: prints out the argument.")
 	return fmt.Errorf(" ")
 }
